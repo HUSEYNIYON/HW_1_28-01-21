14b172b459c4d1ff0aeb34d1aa2aa74040bf8b77
Huseyn <huseyn19.burhonov1999@gmail.com>
2021-01-31 20:50:08 +0500

    Добавьте файлы проекта.

---
+++ HW_1_28-01-21/ArrayHelper.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HW_1_28_01_21
+{
+    static class ArrayHelper
+    {
+        //Method Pop()
+        public static string Pop(ref string[] Arr)
+        {
+            int IndexRemoteElement = Arr.Length - 1;
+            string RemoteElement = Arr[IndexRemoteElement];
+            Arr = Arr.Where((item, index) => index != IndexRemoteElement).ToArray();
+            return RemoteElement;
+        }
+        public static int Pop(ref int[] Arr)
+        {
+            int IndexRemoteElement = Arr.Length - 1;
+            int RemoteElement = Arr[IndexRemoteElement];
+            Arr = Arr.Where((item, index) => index != IndexRemoteElement).ToArray();
+            return RemoteElement;
+        }
+        public static double Pop(ref double[] Arr)
+        {
+            int IndexRemoteElement = Arr.Length - 1;
+            double RemoteElement = Arr[IndexRemoteElement];
+            Arr = Arr.Where((item, index) => index != IndexRemoteElement).ToArray();
+            return RemoteElement;
+        }
+        public static Decimal Pop(ref Decimal[] Arr)
+        {
+            int IndexRemoteElement = Arr.Length - 1;
+            Decimal RemoteElement = Arr[IndexRemoteElement];
+            Arr = Arr.Where((item, index) => index != IndexRemoteElement).ToArray();
+            return RemoteElement;
+        }
+        public static float Pop(ref float[] Arr)
+        {
+            int IndexRemoteElement = Arr.Length - 1;
+            float RemoteElement = Arr[IndexRemoteElement];
+            Arr = Arr.Where((item, index) => index != IndexRemoteElement).ToArray();
+            return RemoteElement;
+        }
+
+
+        //Method Push()
+        public static int Push(ref string[] Arr, string Element)
+        {
+            Arr = Arr.Concat(new string[] { Element }).ToArray();
+            int LengthUpdatedArray = Arr.Length;
+            return LengthUpdatedArray;
+        }
+        public static int Push(ref int[] Arr, int Element)
+        {
+            Arr = Arr.Concat(new int[] { Element }).ToArray();
+            int LengthUpdatedArray = Arr.Length;
+            return LengthUpdatedArray;
+        }
+        public static int Push(ref double[] Arr, double Element)
+        {
+            Arr = Arr.Concat(new double[] { Element }).ToArray();
+            int LengthUpdatedArray = Arr.Length;
+            return LengthUpdatedArray;
+        }
+        public static int Push(ref Decimal[] Arr, Decimal Element)
+        {
+            Arr = Arr.Concat(new Decimal[] { Element }).ToArray();
+            int LengthUpdatedArray = Arr.Length;
+            return LengthUpdatedArray;
+        }
+        public static int Push(ref float[] Arr, float Element)
+        {
+            Arr = Arr.Concat(new float[] { Element }).ToArray();
+            int LengthUpdatedArray = Arr.Length;
+            return LengthUpdatedArray;
+        }
+
+
+        //Method Shift()
+        public static string Shift(ref string[] Arr)
+        {
+            string RemoteElement = Arr[0];
+            Arr = Arr.Where((item, index) => index != 0).ToArray();
+            return RemoteElement;
+        }
+        public static int Shift(ref int[] Arr)
+        {
+            int RemoteElement = Arr[0];
+            Arr = Arr.Where((item, index) => index != 0).ToArray();
+            return RemoteElement;
+        }
+        public static double Shift(ref double[] Arr)
+        {
+            double RemoteElement = Arr[0];
+            Arr = Arr.Where((item, index) => index != 0).ToArray();
+            return RemoteElement;
+        }
+        public static Decimal Shift(ref Decimal[] Arr)
+        {
+            Decimal RemoteElement = Arr[0];
+            Arr = Arr.Where((item, index) => index != 0).ToArray();
+            return RemoteElement;
+        }
+        public static float Shift(ref float[] Arr)
+        {
+            float RemoteElement = Arr[0];
+            Arr = Arr.Where((item, index) => index != 0).ToArray();
+            return RemoteElement;
+        }
+
+
+        //Method UnShift()
+        public static int UnShift(ref string[] Arr, string Element)
+        {
+            var NewArray = new string[Arr.Length + 1];
+            Array.Copy(Arr, 0, NewArray, 1, Arr.Length);
+            int LengthUpdatedArray = NewArray.Length;
+            return LengthUpdatedArray;
+        }
+        public static int UnShift(ref int[] Arr, int Element)
+        {
+            var NewArray = new int[Arr.Length + 1];
+            Array.Copy(Arr, 0, NewArray, 1, Arr.Length);
+            int LengthUpdatedArray = NewArray.Length;
+            return LengthUpdatedArray;
+        }
+        public static int UnShift(ref double[] Arr, double Element)
+        {
+            var NewArray = new double[Arr.Length + 1];
+            Array.Copy(Arr, 0, NewArray, 1, Arr.Length);
+            int LengthUpdatedArray = NewArray.Length;
+            return LengthUpdatedArray;
+        }
+        public static int UnShift(ref Decimal[] Arr, Decimal Element)
+        {
+            var NewArray = new Decimal[Arr.Length + 1];
+            Array.Copy(Arr, 0, NewArray, 1, Arr.Length);
+            int LengthUpdatedArray = NewArray.Length;
+            return LengthUpdatedArray;
+        }
+        public static int UnShift(ref float[] Arr, float Element)
+        {
+            var NewArray = new float[Arr.Length + 1];
+            Array.Copy(Arr, 0, NewArray, 1, Arr.Length);
+            int LengthUpdatedArray = NewArray.Length;
+            return LengthUpdatedArray;
+        }
+    }
+}
+/*Сложности было немного о синтаксисе технологии Linq!
+    Решил данную проблему с помощью сайта metanit.com и оффициальной страницы майкрософт!!!*/
---
+++ HW_1_28-01-21/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace HW_1_28_01_21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] ArrayString = { "1", "2", "3", "4", "5", "6" };
+            int[] ArrayInt = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            double[] ArrayDouble = { 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9 };
+            decimal[] ArrayDecimal = { 1.2m, 1.3m, 1.4m, 1.5m, 1.6m, 1.7m, 1.8m };
+            float[] ArrayFloat = { 1.3f, 1.4f, 1.5f, 1.6f, 1.7f, 1.8f, 1.9f, 1.10f, 1.11f };
+
+            //Console.WriteLine($"Удаленный элемент из массива ArrayString: {ArrayHelper.Pop(ref ArrayString)}");
+            //Console.WriteLine($"Удаленный элемент из массива ArrayInt: {ArrayHelper.Pop(ref ArrayInt)}");
+            //Console.WriteLine($"Удаленный элемент из массива ArrayDouble: {ArrayHelper.Pop(ref ArrayDouble)}");
+            //Console.WriteLine($"Удаленный элемент из массива ArrayDecimal: {ArrayHelper.Pop(ref ArrayDecimal)}");
+            //Console.WriteLine($"Удаленный элемент из массива ArrayFloat: {ArrayHelper.Pop(ref ArrayFloat)}\n\n");
+
+            Console.WriteLine($"Длина массив ArrayString после добавления еще один элемент с конца массива: {ArrayHelper.Push(ref ArrayString, "7")}");
+            Console.WriteLine($"Длина массив ArrayInt после добавления еще один элемент с конца массива: {ArrayHelper.Push(ref ArrayInt, 11)}");
+            Console.WriteLine($"Длина массив ArrayDouble после добавления еще один элемент с конца массива: {ArrayHelper.Push(ref ArrayDouble, 6.0)}");
+            Console.WriteLine($"Длина массив ArrayDecimal после добавления еще один элемент с конца массива: {ArrayHelper.Push(ref ArrayDecimal, 2.8m)}");
+            Console.WriteLine($"Длина массив ArrayFloat после добавления еще один элемент с конца массива: {ArrayHelper.Push(ref ArrayFloat, 7.0f)}\n\n");
+
+            //Console.WriteLine($"Удаленный элемент из массива ArrayString: {ArrayHelper.Shift(ref ArrayString)}");
+            //Console.WriteLine($"Удаленный элемент из массива ArrayInt: {ArrayHelper.Shift(ref ArrayInt)}");
+            //Console.WriteLine($"Удаленный элемент из массива ArrayDouble: {ArrayHelper.Shift(ref ArrayDouble)}");
+            //Console.WriteLine($"Удаленный элемент из массива ArrayDecimal: {ArrayHelper.Shift(ref ArrayDecimal)}");
+            //Console.WriteLine($"Удаленный элемент из массива ArrayFloat: {ArrayHelper.Shift(ref ArrayFloat)}\n\n");
+
+            Console.WriteLine($"Длина массив ArrayString после добавления еще один элемент в начало массива: {ArrayHelper.UnShift(ref ArrayString, "0")}");
+            Console.WriteLine($"Длина массив ArrayInt после добавления еще один элемент начало массива: {ArrayHelper.UnShift(ref ArrayInt, 0)}");
+            Console.WriteLine($"Длина массив ArrayDouble после добавления еще один элемент начало массива: {ArrayHelper.UnShift(ref ArrayDouble, 5.0)}");
+            Console.WriteLine($"Длина массив ArrayDecimal после добавления еще один элемент начало массива: {ArrayHelper.UnShift(ref ArrayDecimal, 2.0m)}");
+            Console.WriteLine($"Длина массив ArrayFloat после добавления еще один элемент начало массива: {ArrayHelper.UnShift(ref ArrayFloat, 6.0f)}\n");
+        }
+    }
+}
